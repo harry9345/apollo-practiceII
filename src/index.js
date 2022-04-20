753e377979cfe180d8c91f97b3e0e11dba95395d
Hossein movahed <hossein.movahed@monese.com>
2022-04-20 10:50:07 +0300

    apollo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import { ApolloClient, HttpLink, InMemoryCache, gql } from '@apollo/client'
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000',
+  })
+})
+
+const query = gql`
+query {
+  allPersons {
+    name,
+    phone,
+    address {
+      street,
+      city
+    }
+    id
+  }
+}
+`
+
+client.query({ query })
+  .then((response) => {
+    console.log(response.data)
+  })
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +39,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
