6fcecd0f3f3cd157626ed0a840ba749d3d433b0d
Hossein movahed <hossein.movahed@monese.com>
2022-04-21 12:03:31 +0300

    part8-3b is finish

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import { gql, useQuery } from '@apollo/client'
+import { useQuery } from '@apollo/client'
 import Persons from './Persons'
-
-const ALL_PERSONS = gql`
-query {
- allPersons {
-  name
-  phone
-  id 
-  }
-}
-`
+import PersonFrom from './PersonFrom'
+import { ALL_PERSONS } from './queries'
+import { useState } from 'react'
+import PhoneForm from './PhoneForm'
 
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
+
   const result = useQuery(ALL_PERSONS)
+
+  const notify = (message)=>{
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000);
+  }
+
   if (result.loading) {
     return <div> loading ...</div>
   }
-  console.log('res', result);
+
   return (
     <div>
+      <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons} />
+      <PersonFrom  setError={notify}/>
+      <PhoneForm  setError={notify} />
+    </div>
+  )
+}
+
+const Notify = ({errorMessage}) =>{
+  if(!errorMessage){
+    return null
+  } 
+  return (
+    <div style={{color:'red'}}>
+      {errorMessage}
     </div>
   )
 }
--- src/Persons.js
+++ src/Persons.js
@@ -1,63 +1,38 @@
 import { useState } from 'react'
-import { gql, useQuery } from '@apollo/client'
-
-const FIND_PERSON = gql`
-  query findPersonByName($nameToSearch: String!) {
-    findPerson(name: $nameToSearch) {
-      name
-      phone
-      id
-      address {
-        street
-        city
-      }
-    }
-  }
-`
-
-const Person = ({ person, onClose }) => {
-  return (
-    <div>
-      <h2>{person.name}</h2>
-      <div>
-        {person.address.street} {person.address.city}
-      </div>
-      <div>{person.phone}</div>
-      <button onClick={onClose}>close</button>
-    </div>
-  )
-}
+import { useQuery } from '@apollo/client'
+import Person from './Person'
+import { FIND_PERSON } from './queries'
 
 const Persons = ({ persons }) => {
-  const [nameToSearch, setNameToSearch] = useState(null)
-  const result = useQuery(FIND_PERSON, {
-    variables: { nameToSearch },
-    skip: !nameToSearch,
-  })
+    const [nameToSearch, setNameToSearch] = useState(null)
+    const result = useQuery(FIND_PERSON, {
+        variables: { nameToSearch },
+        skip: !nameToSearch,
+    })
 
-  if (nameToSearch && result.data) {
+    if (nameToSearch && result.data) {
+        return (
+            <Person
+                person={result.data.findPerson}
+                onClose={() => setNameToSearch(null)}
+            />
+        )
+    }
+
     return (
-      <Person
-        person={result.data.findPerson}
-        onClose={() => setNameToSearch(null)}
-      />
-    )
-  }
+        <div>
+            <h2>Persons</h2>
+            {persons.map((p) => (
+                <div key={p.name}>
+                    {p.name}{' '} {p.phone}{' '}
+                    <button onClick={() => setNameToSearch(p.name)}>
+                        show address
+                    </button>
 
-  return (
-    <div>
-      <h2>Persons</h2>
-      {persons.map((p) => (
-        <div key={p.name}>
-          {p.name} {p.phone} 
-          <button onClick={() => setNameToSearch(p.name)}>
-            show address
-          </button>
-          
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  )
+    )
 }
 
 export default Persons
